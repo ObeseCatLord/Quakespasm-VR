8d43600579f6957f2348852676838b2120ecd35c
obesecatlord <obesecatlord@example.com>
2026-03-23 19:22:27 -0500

    Fix MSVC compilation for debug_log.c and debug_log.h

--- Quake/debug_log.h
+++ Quake/debug_log.h
@@ -1,7 +1,13 @@
 #ifndef DEBUG_LOG_H
 #define DEBUG_LOG_H
 
-void DebugLog(const char *fmt, ...) __attribute__((format(printf, 1, 2)));
+#ifdef __GNUC__
+#define DEBUG_LOG_PRINTF __attribute__((format(printf, 1, 2)))
+#else
+#define DEBUG_LOG_PRINTF
+#endif
+
+void DebugLog(const char *fmt, ...) DEBUG_LOG_PRINTF;
 void DebugLog_Init(void);
 void DebugLog_Close(void);
 
